2aca449673019848201bfb3ee6c99ea649cf3f1b
dromanow <dromanow@mion.elka.pw.edu.pl>
2019-10-17 23:48:17 +0200

    Wczytywanie z pliku

--- src/Network.java
+++ src/Network.java
@@ -1,20 +1,39 @@
-import com.sun.java.swing.plaf.windows.WindowsButtonListener;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import static java.util.Objects.isNull;
 
 public class Network {
-    static long networkCounter = 0;
-    public long networkID;
-    private List<Edge> edgeList;
-    private List<Node> nodeList;
+    private static long networkCounter = 0;
+    private long networkID;
+    private List<Edge> edgeList = new ArrayList<Edge>();
+    private List<Node> nodeList = new ArrayList<Node>();
+    private File file;
+
 
     public Network() {
         this.networkID = networkCounter + 1;
         networkCounter++;
     }
 
-    public void readFromFile(){
+    public void readFromFile(String pathFile) {
+        if(!isNull(pathFile))
+        {
+            try {
+                this.file = new File(pathFile);
+                Scanner sc = new Scanner(file);
+                //TODO: nie koniecznie uzywac tutaj klasy Scanner
+                //TODO: proba odczytania z pliku i zmapowanie zmiennych za pomocą line.split
+                //TODO: trzeba odszukac kluczowych slow WEZLY i LACZA
+                //TODO: nalezy tez uwzglednic omijanie linijek z hashem #
+                }
+                catch(FileNotFoundException e){
+                    System.out.println("Nie znaleziono pliku o podanej sciezce!");
+                }
 
+        }
     }
-
 }
