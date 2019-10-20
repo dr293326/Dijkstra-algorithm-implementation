f9b26dddf81c00c3b3e485cf118221553e9a59b3
filips321 <filipsmejda@icloud.com>
2019-10-20 13:11:37 +0200

    wczytywanie po wezlach i laczach

--- src/Network.java
+++ src/Network.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.LineNumberReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,9 +12,11 @@ import static java.util.Objects.isNull;
 public class Network {
     private static long networkCounter = 0;
     private long networkID;
-    private List<Edge> edgeList = new ArrayList<>();
-    private List<Node> nodeList = new ArrayList<>();
+    private List<Edge> edgeList = null; //ArrayList<>();
+    private List<Node> nodeList = null; // ArrayList<>();
     private File file;
+    private int numberOfNodes;
+    private int numberOfEdges;
 
 
     public Network() {
@@ -23,27 +28,50 @@ public class Network {
         if(!isNull(pathFile))
         {
             try {
-                    this.file = new File(pathFile);
+                this.file = new File(pathFile);
 
-                    if(file.exists()) {
-                        Scanner scanner = new Scanner(file);
-                        while (scanner.hasNextLine()){
+                if (file.exists()) {
 
-                            //Zaciagam linie
-                            String line = scanner.nextLine();
+                    Scanner scanner = new Scanner(file);
+                    while (scanner.hasNextLine()) {
 
-                            //Pomijam linie z #
-                            if (!(line.substring(0,1).contains("#"))){
-                                //Rozdzielam kazda linie za pomoca regexa w split
-                                //Otrzymuje tablice stringow ktore byly rodzielone spacja
-                                String[] result = line.split(" ");
+                        //Zaciagam linie
+                        String line = scanner.nextLine();
 
+                        //Pomijam linie z #
+                        if (!(line.substring(0, 1).contains("#"))) {
+                            //Rozdzielam kazda linie za pomoca regexa w split
+                            //Otrzymuje tablice stringow ktore byly rodzielone spacja
+                            String[] result = line.split(" ");
 
+                            switch (result[0]) {
+                                case "WEZLY":
+                                    numberOfNodes = Integer.parseInt(result[2]);
+                                    nodeList = new ArrayList<>(numberOfNodes);
+                                    for (int i = 0; i < numberOfNodes; i++) {
+                                        line = scanner.nextLine();
+                                            Node tmp = new Node(Long.parseLong(result[0]), Integer.parseInt(result[1]),
+                                                    Integer.parseInt(result[2]));
+                                            nodeList.add(tmp);
+                                        }
+                                    break;
+
+                                /*case "LACZA":
+                                    numberOfEdges = Integer.parseInt(result[2]);
+                                    for (int i = 0; i < numberOfEdges; i++) {
+                                        Edge tmp = new Edge(Long.parseLong(result[0]), 0, "nie wiem",
+                                                Integer.parseInt(result[1]), Integer.parseInt(result[1]));
+                                        edgeList.add(tmp);
+                                    }
+                                    break;*/
                             }
                         }
+
                     }
-                    //TODO: trzeba odszukac kluczowych slow WEZLY i LACZA
                 }
+            }
+
+                    //TODO: trzeba odszukac kluczowych slow WEZLY i LACZA
                 catch(FileNotFoundException ex){
                     System.out.println("Nie znaleziono pliku o podanej sciezce!");
                 }
